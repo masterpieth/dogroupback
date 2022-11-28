c6a151ef822d9f6a27c26b632b5f971224d82834
26 <adorableyou26@gmail.com>
2022-11-28 16:34:08 +0900

    2022-11-28 성찬민 가입기능 구현(insertUser) (#8)
    * 2022-11-28 성찬민 가입기능 구현(insertUser)
    
    * 2022-11-28 김나영 sysout 제거
    
    Co-authored-by: NYK <kny0339@gmail.com>

--- dogroupback/src/main/java/com/my/repository/UserRepositoryOracle.java
+++ dogroupback/src/main/java/com/my/repository/UserRepositoryOracle.java
@@ -1,5 +1,35 @@
 package com.my.repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.my.dto.UserDTO;
+import com.my.sql.MyConnection;
+
 public class UserRepositoryOracle implements UserRepository {
+	private Connection conn = null;
+	private PreparedStatement preStmt = null;
+	private ResultSet rs = null;
+	
+	@Override
+	public void insertUser(UserDTO inputUser) {
+		//기본 성실도:50, 잔액:0, 상태:1
+		String insertUserSQL = "insert into users(user_email, user_name, user_password, user_diligence, user_balance, user_status)"
+							+ " values(?, ?, ?, 50, 0, 1)";
+		try {
+			conn =  MyConnection.getConnection();
+			preStmt = conn.prepareStatement(insertUserSQL);
+			preStmt.setString(1, inputUser.getEmail());
+			preStmt.setString(2, inputUser.getName());
+			preStmt.setString(3, inputUser.getPassword());
+			preStmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			MyConnection.close(preStmt, conn);
+		}
+		
+	}
 
 }
--- dogroupback/src/main/java/com/my/service/StudyService.java
+++ dogroupback/src/main/java/com/my/service/StudyService.java
@@ -1,5 +1,41 @@
 package com.my.service;
 
-public class StudyService {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Properties;
 
+import com.my.repository.StudyRepository;
+
+public class StudyService {
+	private StudyRepository repository;
+
+	public StudyService(String propertiesFileName) {
+		Properties env = new Properties();
+		try {
+			env.load(new FileInputStream(propertiesFileName));
+			String className = env.getProperty("study");
+			Class clazz = Class.forName(className);
+			Object obj = clazz.getDeclaredConstructor().newInstance();
+			repository = (StudyRepository)obj;
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
 }
