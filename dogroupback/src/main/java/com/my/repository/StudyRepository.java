6b44c9bf749ee063d2d9d7a2af8fd84d0b78d32b
NYK <kny0339@gmail.com>
2022-11-28 18:13:49 +0900

    2022-11-28 김나영 insertHomeworkByEmail, checkMyGithubCommit 구현

--- dogroupback/src/main/java/com/my/repository/StudyRepository.java
+++ dogroupback/src/main/java/com/my/repository/StudyRepository.java
@@ -1,8 +1,10 @@
 package com.my.repository;
 
+import java.sql.Date;
 import java.util.List;
 
 import com.my.dto.StudyDTO;
+import com.my.exception.AddException;
 import com.my.exception.FindException;
 
 public interface StudyRepository {
@@ -13,7 +15,14 @@ public interface StudyRepository {
 	 * @throws FindException
 	 */
 	List<StudyDTO> selectStudyByEmail(String email) throws FindException;
-	
+	/**
+	 * 과제를 테이블에 추가한다. 오늘날짜의 과제가 없으면 AddException을 터뜨린다.
+	 * @param email				//회원Email
+	 * @param studyId			//스터디Id
+	 * @param created_at 		//이벤트생성일자
+	 * @throws AddException		//데이터를 넣을 수 없을 때 발생하는 예외
+	 */
+	void insertHomeworkByEmail(String email, int studyId, Date created_at) throws AddException;
 }
 
 
