a2c6a2ff757922697ab1922cdb6ba07780691d65
thkim <qkek2661@naver.com>
2022-11-28 14:16:29 +0900

    2022-11-28 김태현 selectStudyByEmail작업중

--- dogroupback/src/main/java/com/my/repository/StudyRepository.java
+++ dogroupback/src/main/java/com/my/repository/StudyRepository.java
@@ -1,5 +1,21 @@
 package com.my.repository;
 
-public interface StudyRepository {
+import java.util.List;
 
+import com.my.dto.StudyDTO;
+import com.my.exception.FindException;
+
+public interface StudyRepository {
+	
+	
+	/**
+	 * @param email 스터디 id
+	 * @return 진행중인 스터디를 검색한다.
+	 * @throws FindException
+	 */
+	List<StudyDTO> selectStudyByEmail(String email) throws FindException;
+	
 }
+
+
+
--- dogroupback/src/main/java/com/my/repository/StudyRepositoryOracle.java
+++ dogroupback/src/main/java/com/my/repository/StudyRepositoryOracle.java
@@ -1,5 +1,47 @@
 package com.my.repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.my.dto.StudyDTO;
+import com.my.exception.FindException;
+import com.my.sql.MyConnection;
+
 public class StudyRepositoryOracle implements StudyRepository {
 
+	public List<StudyDTO> selectStudyByEmail(String email) throws FindException {
+		List<StudyDTO> list = new ArrayList<>();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = MyConnection.getConnection();
+			String selectStudyByEmailSQL = "SELECT * FROM STUDY";
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+		try {
+			pstmt = conn.prepareStatement(selectStudyByEmailSQL);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
 }
+
