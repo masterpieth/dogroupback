64d48c729bdb954b039ffdb3c2c6d7d0fdab5c12
kangbm92 <kangbm92@gmail.com>
2022-11-28 18:04:30 +0900

    Revert "2022-11-28 강보미 idDuplicateCheck/selectUserByEmail 메소드구현"
    This reverts commit 1974b09431819f9b5c69837747501fbd1e353f6c.

--- dogroupback/src/main/java/com/my/service/UserService.java
+++ dogroupback/src/main/java/com/my/service/UserService.java
@@ -1,22 +1,5 @@
 package com.my.service;
 
-import java.sql.SQLException;
-
-import com.my.exception.FindException;
-import com.my.repository.UserRepository;
-
 public class UserService {
-	private UserRepository repository;
 
-	
-	
-	
-	public void  idDuplicateCheck(String email) throws Exception{
-		
-		repository.selectUserByEmail(email);
-		
-		
-	}
-	
-	
 }
