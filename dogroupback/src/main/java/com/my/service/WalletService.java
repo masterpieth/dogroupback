41405d0382fdf799175063ac7f3e499e86a6b91c
kangbm92 <kangbm92@gmail.com>
2022-11-30 20:02:40 +0900

    22-11-30 강보미 WalletRepository의 updateUserBalance메소드생성/ WalletRepositoryOracle의 updateUserBalance메소드구현/ WalletService의 deposit메소드와 withdraw메소드 로직구현

--- dogroupback/src/main/java/com/my/service/WalletService.java
+++ dogroupback/src/main/java/com/my/service/WalletService.java
@@ -6,7 +6,9 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import java.util.Properties;
 
+import com.my.dto.UserDTO;
 import com.my.dto.WalletDTO;
+import com.my.exception.AddException;
 import com.my.exception.FindException;
 import com.my.repository.WalletRepository;
 
@@ -57,6 +59,23 @@ public class WalletService {
 	 * @throws Exception
 	 */
 	public void deposit(WalletDTO wallet) throws Exception {
-		
+		wallet.setTransactionCategory(3);
+		repository.updateUserBalance(wallet, 1);
+	}
+
+	/**
+	 * 
+	 * @param wallet 사용자의 지갑에 있는 돈을 출금한다
+	 * @throws Exception
+	 */
+	public void withdraw(WalletDTO wallet) throws Exception {
+		UserService us = new UserService("repository.properties");
+		UserDTO userDTO = us.searchUserInfo(wallet.getEmail());
+		if (userDTO.getUserBalance() > wallet.getTransactionMoney()) {
+			wallet.setTransactionCategory(4);
+			repository.updateUserBalance(wallet, 0);
+		} else {
+			throw new AddException();
+		}
 	}
 }
