dc7b802db19a723f9a804cf342193e87af113804
김태현 <qkek2661@naver.com>
2022-11-30 18:53:06 +0900

    2022-11-30 김태현 PageBean 풀리퀘스트 (#31)
    * 2022-11-30 김태현 pagebean 커밋
    
    * 2022-11-30 김태현 페이지빈 커밋

--- dogroupback/src/main/java/com/my/repository/StudyRepository.java
+++ dogroupback/src/main/java/com/my/repository/StudyRepository.java
@@ -37,6 +37,7 @@ public interface StudyRepository {
 
 	/**
 	 * 스터디원의 email로 스터디원의 List<HomeworkDTO> 리스트 반환한다.
+	 * 
 	 * @param email
 	 * @param sstudyId
 	 * @return 스터디원의 과제 리스트
@@ -46,22 +47,26 @@ public interface StudyRepository {
 
 	/**
 	 * 스터디의 모든 정보를 반환한다.
+	 * 
 	 * @param studyId
-	 * @return 스터디 기본 정보 + 스터디의 현재 참여자수 + 스터디장의 성실도 + 스터디의 과목 정보 + 참여중인 스터디원들의 정보(유저 기본 정보 + 과제 제출 + 유효 출석일)
+	 * @return 스터디 기본 정보 + 스터디의 현재 참여자수 + 스터디장의 성실도 + 스터디의 과목 정보 + 참여중인 스터디원들의 정보(유저
+	 *         기본 정보 + 과제 제출 + 유효 출석일)
 	 * @throws FindException 상세 조회중 오류시 발생
 	 */
 	StudyDTOBomi selectStudy(int studyId) throws FindException;
 
 	/**
 	 * 스터디에 해당하는 제출한 과제를 반환한다.
+	 * 
 	 * @param studyId 스터디아이디
-	 * @return 
+	 * @return
 	 * @throws FindException
 	 */
 	List<HomeworkDTO> selectHomeworkByStudyId(int studyId) throws FindException;
-	
+
 	/**
 	 * 스터디ID로 스터디의 기본정보를 반환한다.
+	 * 
 	 * @param studyId
 	 * @return 스터디 기본정보
 	 * @throws FindException
@@ -70,46 +75,64 @@ public interface StudyRepository {
 
 	/**
 	 * 스터디를 Insert 한다
-	 * @param study			추가할 스터디 내용
+	 * 
+	 * @param study 추가할 스터디 내용
 	 * @throws AddException 추가 중 오류시 발생한다.
 	 */
 	void insertStudy(StudyDTOBomi study) throws AddException;
-	
+
 	/**
 	 * 스터디를 update한다
-	 * @param study				업데이트할 스터디 내용
-	 * @throws ModiftException 	업데이트 중 오류시 발생한다.
+	 * 
+	 * @param study 업데이트할 스터디 내용
+	 * @throws ModiftException 업데이트 중 오류시 발생한다.
 	 */
 	void updateStudy(StudyDTO study) throws ModifyException;
+
 	/**
-	 * 스터디원을 Insert 한다 - 지갑 관련 추가-삭제 등을 담당하는 프로시저를 호출한다.
-	 * flag 1: 스터디원 추가
-	 * flag 0: 스터디원 삭제
+	 * 스터디원을 Insert 한다 - 지갑 관련 추가-삭제 등을 담당하는 프로시저를 호출한다. flag 1: 스터디원 추가 flag 0:
+	 * 스터디원 삭제
 	 */
 	void insertStudyUser(StudyDTO study, String email) throws AddException;
 
 	/**
-	 * 스터디장을 insert 한다 - insertStudy에서 connection을 받아서 한 트랜잭션에 있도록 한다.
-	 * flag 1: 스터디원 추가
-	 * flag 0: 스터디원 삭제
+	 * 스터디장을 insert 한다 - insertStudy에서 connection을 받아서 한 트랜잭션에 있도록 한다. flag 1: 스터디원
+	 * 추가 flag 0: 스터디원 삭제
 	 */
 	void insertStudyUserLeader(StudyDTO study, String email, Connection conn) throws AddException;
-	
+
 	/**
 	 * 스터디에서 탈퇴한다 - StudyUser 테이블에서 정보를 제거한다.
-	 * @param study				스터디정보
-	 * @param email				사용자이메일
+	 * 
+	 * @param study 스터디정보
+	 * @param email 사용자이메일
 	 * @throws RemoveException
 	 */
 	void deleteStudyUser(StudyDTO study, String email) throws RemoveException;
 
 	/**
 	 * 스터디 과목을 insert 한다
-	 * @param study				스터디 정보
-	 * @param subjects			스터디과목들
-	 * @param conn				insertStudy시의 connection
-	 * @throws AddException		실패시 발생시킬 예외
+	 * 
+	 * @param study    스터디 정보
+	 * @param subjects 스터디과목들
+	 * @param conn     insertStudy시의 connection
+	 * @throws AddException 실패시 발생시킬 예외
 	 */
-	void insertStudySubject(StudyDTOBomi study, List<StudySubjectDTOBomi> subjects, Connection conn) throws AddException;
-}
+	void insertStudySubject(StudyDTOBomi study, List<StudySubjectDTOBomi> subjects, Connection conn)
+			throws AddException;
 
+	/**
+	 * 조건에 맞는 스터디 개수를 카운트하여 반환한다. (조건 : 타이틀명, 스터디 정원)
+	 * 
+	 * @return 개수를 반환
+	 * @throws FindException
+	 */
+	int studyCount(String studyTitle, int studySize) throws FindException;
+
+	/**
+	 * 검색 조건에 맞는 스터디 리스트를 반환한다. (조건 : 현재 페이지, 페이지당 개수, 타이틀명, 스터디 정원)
+	 * 
+	 * @return 스터디
+	 */
+	List<StudyDTO> selectStudy(int currentPage, int cntPerPage, String studyTitle, int studySize) throws FindException;
+}
--- dogroupback/src/main/java/com/my/repository/StudyRepositoryOracle.java
+++ dogroupback/src/main/java/com/my/repository/StudyRepositoryOracle.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -24,7 +25,7 @@ import com.my.exception.RemoveException;
 import com.my.sql.MyConnection;
 
 public class StudyRepositoryOracle implements StudyRepository {
-	
+
 	@Override
 	public List<StudyDTO> selectStudyByEmail(String email) throws FindException {
 		List<StudyDTO> list = new ArrayList<>();
@@ -33,13 +34,12 @@ public class StudyRepositoryOracle implements StudyRepository {
 		ResultSet rs = null;
 		try {
 			conn = MyConnection.getConnection();
-			String selectStudyByEmailSQL = "SELECT * FROM study WHERE user_email= ? ";
+			String selectStudyByEmailSQL = "SELECT s.* FROM study s JOIN USERS u ON(s.user_email = u.user_email)WHERE s.user_email = ?";
 
 			preStmt = conn.prepareStatement(selectStudyByEmailSQL);
 			preStmt.setString(1, email);
 			rs = preStmt.executeQuery();
 
-			System.out.println("연결되었음");
 			while (rs.next()) {
 				int studyId = rs.getInt("study_id");
 				String userEmail = rs.getString("user_email");
@@ -96,9 +96,10 @@ public class StudyRepositoryOracle implements StudyRepository {
 		}
 	}
 
-	
 	/**
-	 * 스터디의 모든 과제 제출 내역을 리스트로 반환한다.
+	 * 스터디의 모든 과제 제출 내역을 리스트로 반환한다. selectStudy스터디 목록조회(회원용) study, study_users,
+	 * study_subject, subject테이블사용 현재 해당 스터디에 참가중인 회원수를 포함하여 보여줌 하나의 studyId로 검색했을때
+	 * 서로다른 스터디과목명은 3개가 온다.
 	 */
 	@Override
 	public List<HomeworkDTO> selectHomeworkByStudyId(int studyId) throws FindException {
@@ -177,7 +178,6 @@ public class StudyRepositoryOracle implements StudyRepository {
 				u.setDiligence(rs.getInt("diligence"));
 				u.setEmail(rs.getString("user_email"));
 				study.setStudyLeader(u);
-
 				StudySubjectDTOBomi studySubjectDtoBomi = new StudySubjectDTOBomi();
 				SubjectDTOBomi subjectDToBomi = new SubjectDTOBomi(rs.getString("subject_code"),
 						rs.getString("subject_name"), null);
@@ -210,7 +210,7 @@ public class StudyRepositoryOracle implements StudyRepository {
 					user.setPassword(rs.getString("user_password"));
 					user.setStatus(rs.getInt("user_status"));
 					user.setUserBalance(rs.getInt("user_balance"));
-					user.setHomeworkList(selectUserHomeworkByEmail(rs.getString("user_email"), studyId)); 
+					user.setHomeworkList(selectUserHomeworkByEmail(rs.getString("user_email"), studyId));
 					studyUserList.add(user);
 				}
 				study.setStudyUsers(studyUserList);
@@ -226,8 +226,7 @@ public class StudyRepositoryOracle implements StudyRepository {
 			MyConnection.close(rs, preStmt, conn);
 		}
 	}
-	
-	
+
 	/**
 	 * 스터디 정보를 반환한다.
 	 * 
@@ -279,13 +278,11 @@ public class StudyRepositoryOracle implements StudyRepository {
 		}
 	}
 
-	/*
+	/**
 	 * 스터디 유저의(개인) 모든 과제 제출 내역을 반환한다.
 	 * 
 	 * @parama userEmail 유저 email
-	 * 
 	 * @param studyId 스터디 ID
-	 * 
 	 * @return 과제 리스트
 	 */
 	@Override
@@ -319,7 +316,7 @@ public class StudyRepositoryOracle implements StudyRepository {
 			MyConnection.close(rs, preStmt, conn);
 		}
 	}
-	
+
 	/**
 	 * 스터디를 Insert 한다. Study와 StudyUser(스터디장)가 한 트랜잭션에 insert되고, 실패시 롤백한다.
 	 */
@@ -350,7 +347,7 @@ public class StudyRepositoryOracle implements StudyRepository {
 			clob.setString(1, study.getStudyContent());
 			preStmt.setClob(10, clob);
 			preStmt.executeUpdate();
-			
+
 			preStmt = conn.prepareStatement(insertedSeqSQL);
 			rs = preStmt.executeQuery();
 			if (rs.next()) {
@@ -359,7 +356,7 @@ public class StudyRepositoryOracle implements StudyRepository {
 			// 스터디장 insert
 			insertStudyUserLeader(study, study.getUserEmail(), conn);
 			// 스터디 과목 insert
-			insertStudySubject(study, study.getSubjects(), conn); 
+			insertStudySubject(study, study.getSubjects(), conn);
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
@@ -373,7 +370,7 @@ public class StudyRepositoryOracle implements StudyRepository {
 	@Override
 	public void updateStudy(StudyDTO study) throws ModifyException {
 	}
-	
+
 	/**
 	 * 스터디장을 insert 한다 - insertStudy에서 connection을 받아서 한 트랜잭션에 있도록 한다.
 	 * 
@@ -382,6 +379,7 @@ public class StudyRepositoryOracle implements StudyRepository {
 	public void insertStudyUserLeader(StudyDTO study, String email, Connection conn) throws AddException {
 		CallableStatement calStmt = null;
 		ResultSet rs = null;
+		PreparedStatement preStmt = null;
 		try {
 			// 지갑 관련 프로시저
 			String procSQL = "{ call proc_wallet(?, ?, ?, ?, ?, ?) }";
@@ -393,6 +391,11 @@ public class StudyRepositoryOracle implements StudyRepository {
 			calStmt.setInt(5, 2);
 			calStmt.setInt(6, study.getStudyFee());
 			calStmt.executeUpdate();
+			// 유저 insert
+			String insertStudyUserSQL = "INSERT INTO STUDY_USERS VALUES(?, ?)";
+			preStmt = conn.prepareStatement(insertStudyUserSQL);
+			preStmt.setInt(1, study.getStudyId());
+			preStmt.setString(2, email);
 			conn.commit();
 		} catch (Exception e) {
 			try {
@@ -403,6 +406,14 @@ public class StudyRepositoryOracle implements StudyRepository {
 			}
 		} finally {
 			MyConnection.close(rs, calStmt, null);
+			MyConnection.close(rs, preStmt, conn);
+			if (calStmt != null) {
+				try {
+					calStmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 
@@ -429,6 +440,11 @@ public class StudyRepositoryOracle implements StudyRepository {
 			calStmt.setInt(5, 2);
 			calStmt.setInt(6, study.getStudyFee());
 			calStmt.executeUpdate();
+			// 유저 insert
+			String insertStudyUserSQL = "INSERT INTO STUDY_USERS VALUES(?, ?)";
+			preStmt = conn.prepareStatement(insertStudyUserSQL);
+			preStmt.setInt(1, study.getStudyId());
+			preStmt.setString(2, email);
 			conn.commit();
 		} catch (Exception e) {
 			try {
@@ -471,6 +487,11 @@ public class StudyRepositoryOracle implements StudyRepository {
 			calStmt.setInt(5, 5);
 			calStmt.setInt(6, study.getStudyFee());
 			calStmt.executeUpdate();
+			// 스터디 유저를 삭제한다.
+			String deleteStudyUserSQL = "DELETE FROM study_users WHERE study_id = ? AND user_email = ?";
+			preStmt = conn.prepareStatement(deleteStudyUserSQL);
+			preStmt.setInt(1, study.getStudyId());
+			preStmt.setString(2, email);
 			conn.commit();
 		} catch (Exception e) {
 			try {
@@ -491,19 +512,19 @@ public class StudyRepositoryOracle implements StudyRepository {
 		}
 	}
 
-	
 	/**
 	 * 스터디 과목을 insert 한다 - insertStudy에서 connection을 받아서 한 트랜잭션에 있도록 한다.
 	 */
 	@Override
-	public void insertStudySubject(StudyDTOBomi study, List<StudySubjectDTOBomi> subjects, Connection conn) throws AddException {
+	public void insertStudySubject(StudyDTOBomi study, List<StudySubjectDTOBomi> subjects, Connection conn)
+			throws AddException {
 		PreparedStatement preStmt = null;
 		ResultSet rs = null;
 		try {
-			//스터디 과목 insert
+			// 스터디 과목 insert
 			String insertStudySubjectSQL = "INSERT INTO STUDY_SUBJECT VALUES(?, ?)";
 			preStmt = conn.prepareStatement(insertStudySubjectSQL);
-			for(StudySubjectDTOBomi subject : subjects) {
+			for (StudySubjectDTOBomi subject : subjects) {
 				preStmt.setInt(1, study.getStudyId());
 				preStmt.setString(2, subject.getSubject().getSubjectCode());
 				preStmt.executeUpdate();
@@ -520,5 +541,100 @@ public class StudyRepositoryOracle implements StudyRepository {
 		}
 	}
 
-	
+	/**
+	 * 검색 조건에 맞는 스터디 개수를 카운트하여 반환한다. (조건 : 타이틀명, 스터디 정원)
+	 */
+	@Override
+	public int studyCount(String studyTitle, int studySize) throws FindException {
+		Connection conn = null;
+		PreparedStatement preStmt = null;
+		ResultSet rs = null;
+		String studyStudyCountSQL = null;
+		try {
+			conn = MyConnection.getConnection();
+			if (studyTitle == null)
+				studyTitle = "";
+			if (studySize != 0) {
+				studyStudyCountSQL = "SELECT count(*) FROM study WHERE study_title Like ? and study_size = ?";
+				preStmt = conn.prepareStatement(studyStudyCountSQL);
+				preStmt.setString(1, "%" + studyTitle + "%");
+				preStmt.setInt(2, studySize);
+			} else {
+				studyStudyCountSQL = "SELECT count(*) FROM study WHERE study_title Like ? ";
+				preStmt = conn.prepareStatement(studyStudyCountSQL);
+				preStmt.setString(1, "%" + studyTitle + "%");
+			}
+			rs = preStmt.executeQuery();
+			rs.next();
+			int count = rs.getInt(1);
+			return count;
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new FindException(e.getMessage());
+		} finally {
+			MyConnection.close(rs, preStmt, conn);
+		}
+	}
+
+	/**
+	 * 검색 조건에 맞는 스터디 리스트를 반환한다. (조건 : 현재 페이지, 페이지당 개수, 타이틀명, 스터디 정원)
+	 */
+	@Override
+	public List<StudyDTO> selectStudy(int currentPage, int cntPerPage, String studyTitle, int studySize)
+			throws FindException {
+		Connection conn = null;
+		PreparedStatement preStmt = null;
+		ResultSet rs = null;
+		List<StudyDTO> list = new ArrayList<>();
+		String studyStudySQL = null;
+		int startRow = currentPage * cntPerPage - cntPerPage + 1;
+		int endRow = currentPage * cntPerPage;
+		try {
+			conn = MyConnection.getConnection();
+			if (studyTitle == null)
+				studyTitle = "";
+			if (studySize != 0) {
+				studyStudySQL = "SELECT * FROM  ( SELECT rownum rn, a.* FROM (SELECT * FROM study WHERE study_title Like ? and study_size = ? order by study_id) a )WHERE rn BETWEEN ? AND ?";
+				preStmt = conn.prepareStatement(studyStudySQL);
+				preStmt.setString(1, "%" + studyTitle + "%");
+				preStmt.setInt(2, studySize);
+				preStmt.setInt(3, startRow);
+				preStmt.setInt(4, endRow);
+			} else {
+				studyStudySQL = "SELECT * FROM  ( SELECT rownum rn, a.* FROM (SELECT * FROM study WHERE study_title Like ? order by study_id) a )WHERE rn BETWEEN ? AND ?";
+				;
+				preStmt = conn.prepareStatement(studyStudySQL);
+				preStmt.setString(1, "%" + studyTitle + "%");
+				preStmt.setInt(2, startRow);
+				preStmt.setInt(3, endRow);
+			}
+			rs = preStmt.executeQuery();
+			while (rs.next()) {
+				int studyId = rs.getInt("study_id");
+				String userEmail = rs.getString("user_email");
+				studyTitle = rs.getString("study_title");
+				studySize = rs.getInt("study_size");
+				int studyFee = rs.getInt("study_fee");
+				int studyCertification = rs.getInt("study_certification");
+				int studyDiligenceCutline = rs.getInt("study_diligence_cutline");
+				Date studyPostDate = rs.getDate("study_post_date");
+				Date studyStartDate = rs.getDate("study_start_date");
+				Date studyEndDate = rs.getDate("study_end_date");
+				int studyHomeworkPerWeek = rs.getInt("STUDY_HOMEWORK_PER_WEEK");
+				int studyPaid = rs.getInt("STUDY_PAID");
+				String studyContent = rs.getString("STUDY_CONTENT");
+				StudyDTO studyall = new StudyDTO(studyId, userEmail, studyTitle, studySize, studyFee,
+						studyCertification, studyDiligenceCutline, studyPostDate, studyStartDate, studyEndDate,
+						studyHomeworkPerWeek, studyPaid, studyContent);
+				list.add(studyall);
+			}
+			return list;
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new FindException(e.getMessage());
+		} finally {
+			MyConnection.close(rs, preStmt, conn);
+		}
+	}
+
 }
